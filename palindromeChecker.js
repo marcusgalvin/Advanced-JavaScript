81cc0491d1a1c0dc9e191563a9b2d4a065a0555d
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-12-13 13:13:49 -0500

    solved

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,14 +1,26 @@
 function palindrome(str) {
   //lower case all and remove spaces from str
-  str = str.toLowerCase('').replace(/\s/g, '');
-  console.log(str)
+  str = str.toLowerCase('').replace(/[^A-Z0-9]/gi, "");
+  console.log('original:', str)
 
   var reverse = str.split('').reverse('').join('');
   console.log('reversed:', reverse)
 
   for (var i = 0; i < str.length; i++) {
     for (var j = 0; j < reverse.length; j++) {
+      // console.log(reverse[j])
 
-      // if (str[i])
+      if (str[i] == reverse[j]) {
+        return true;
+      }
+      else {
+        return false;
+      }
     }
   }
+}
+
+// palindrome("eye");
+// palindrome("not a palindrome");
+// palindrome("My age is 0, 0 si ega ym.")
+palindrome("almostomla");
